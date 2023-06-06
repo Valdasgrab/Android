a3095d09d425c249ec5665f8b3cd637720f6a4a9
vgrabauskas <valdasgrab@gmail.com>
2023-06-06 15:01:49 +0300

    Relocate MainActivity

--- app/src/main/java/lt/vgrabauskas/androidtopics/common/MainActivity.kt
+++ app/src/main/java/lt/vgrabauskas/androidtopics/common/MainActivity.kt
@@ -1,7 +1,8 @@
-package lt.vgrabauskas.androidtopics
+package lt.vgrabauskas.androidtopics.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import lt.vgrabauskas.androidtopics.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
